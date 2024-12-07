30736c4381d0c7b126acc5ffcf08548722ffc7cf
ivameri <ivamerii@yandex.ru>
2024-12-07 23:59:06 +0200

    creare Message model, MessageAdmin

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 
+from tickets.models.messages import Message
 from tickets.models.tickets import Ticket, TicketStatus
 
 
 @admin.register(TicketStatus)
 class TicketStatusAdmin(admin.ModelAdmin):
-    list_display = ('code', 'name', )
-    list_display_links = ('code', )
+    list_display = ('code', 'name',)
+    list_display_links = ('code',)
 
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('client', 'status', 'created_at', 'manager')
     list_display_links = ('client',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'user', 'created_at', 'message')
+    list_display_links = ('ticket',)
+
