6b116ddadf4b3670313eb7b92a9f6845b5dba84e
ivameri <ivamerii@yandex.ru>
2024-12-08 10:14:43 +0200

    add assing-to-me

--- tickets/views/tickets.py
+++ tickets/views/tickets.py
@@ -1,10 +1,12 @@
 from drf_spectacular.utils import extend_schema_view, extend_schema
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from tickets.models.tickets import Ticket
+from tickets.models.tickets import Ticket, TicketStatus
 from tickets.permissions import IsManager
 from tickets.serializers.tickets import TicketSerializer
 
@@ -19,3 +21,24 @@ class TicketView(GenericViewSet, mixins.ListModelMixin):
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('status',)
     ordering = ('created_at',)
+
+    @extend_schema(
+        summary='Назначить обращение текущему менеджеру',
+        tags=['Обращения'],
+        request=None,
+    )
+    @action(methods=['POST'], detail=True, url_path='assign-to-me')
+    def assign_to_me(self, request, pk=None):
+        ticket = self.get_object()
+
+        if ticket.manager is not None:
+            return Response({'error': 'Ticket already assigned'}, status=400)
+
+        if request.user.role != 'manager':
+            return Response({'error': 'Permission denied'}, status=403)
+
+        ticket.manager = request.user
+        ticket.status = TicketStatus.objects.filter(code='in_progress').first()
+        ticket.save()
+
+        return Response({'message': 'Ticket assigned successfully'})
